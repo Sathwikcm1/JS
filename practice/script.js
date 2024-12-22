5ae3cb96af20785ae98f296b8c5a8323c1ce493e
Sathwikcm1 <sathwikcm@gmail.com>
2024-12-22 16:25:24 +0530

    Initial_Commit

---
+++ practice/script.js
@@ -0,0 +1,6 @@
+document.getElementById("changeText").addEventListener("click", () => {
+  document.getElementById("greeting").textContent = "You clicked the button!";
+});
+
+console.log("hello there")
+let x=10; console.log(x);
